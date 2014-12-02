f4ed5cc8180a57e05b2ba1657e6659fc189eafa6
Erik Sjöström <metallicgeek@gmail.com>
2014-12-02 15:14:07 +0100

    Fixade ett script som borde funka

--- doorchecker.py
+++ doorchecker.py
@@ -1,22 +1,61 @@
-#!/usr/bin/python3
-import serial
 
-% Establish a serial connection with the Arduino.  
-% Change '/dev/ttyACM0' to whatever is appropriate.
-ser = serial.Serial('/dev/ttyACM0',9600)
+def main():
+	from time import sleep
+	import serial
 
-% The arduino resets every time a serial connection is established and the
-% following is somewhat of a workaround. 
-% Reset can be disabled by placing a 120 ohm resistor between reset and gnd.
-%
-% The workaround is that we wait for the Arduino to reset, and to send a sign 
-% of life. Then we send it an arbitrary char to let it know that we are able
-% recieve. 
- 
-x = ser.readline()
-ser.write(b'A')
+	oldStatus = False
+	newStatus = False
 
-% The Arduino sends data in bytes, so we decode into utf-8
-print(ser.readline().decode('utf-8'),end='')
-ser.close();
+	def write_to_file(var):
+		try:
+			f = open("index.html", "w")
+			f.write("<html>\
+					 <head>\
+					 <meta charset=\"utf-8\">\
+					 <title></title>\
+					 <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\
+					 </head>\
+					 <body>\
+					 <p>" + var + "</p>\
+					 </body>\
+					 </html>")
+		except Exception as e:
+			raise e
+		finally:
+			f.close()
 
+	def get_status():
+		# Establish a serial connection with the Arduino.  
+		# Change '/dev/ttyACM0' to whatever is appropriate.
+		ser = serial.Serial('/dev/ttyACM0',9600)
+
+		# The arduino resets every time a serial connection is established and the
+		# following is somewhat of a workaround. 
+		# Reset can be disabled by placing a 120 ohm resistor between reset and gnd.
+		#
+		# The workaround is that we wait for the Arduino to reset, and to send a sign 
+		# of life. Then we send it an arbitrary char to let it know that we are able
+		# recieve. 
+		 
+		x = ser.readline()
+		ser.write(b'A')
+
+		# The Arduino sends data in bytes, so we decode into utf-8
+		status = ser.readline().decode('utf-8')
+		ser.close();
+		return status
+
+	while True:
+		try:
+			newStatus = str(get_status())
+		except Exception as e:
+			raise e
+		if oldStatus != newStatus:
+			write_to_file(newStatus)
+			oldStatus = newStatus
+		else:
+			pass
+		sleep(60)
+
+if __name__ == '__main__':
+	main()
